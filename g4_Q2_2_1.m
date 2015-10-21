93b66a5e0a783170b809b41b112ce5ba99953f0d
Masaki Sugihara <13ec050@ms.dendai.ac.jp>
2015-10-21 11:03:45 +0900

    for文

---
+++ g4_Q2_2_1.m
@@ -0,0 +1,7 @@
+%Q.2.2.1
+clear all; close all
+sum=0;
+for ii=1:100
+    sum=sum+ii;
+end
+sum
---
+++ g4_Q2_2_3.m
@@ -0,0 +1,12 @@
+%Q2_2_3
+
+clear all;close all
+
+%‡@
+xx = [2,4,8,5,1]
+%‡A
+sum = 0;
+for ii = 1:5
+    sum = sum + xx(ii);
+end
+ave = sum/5
